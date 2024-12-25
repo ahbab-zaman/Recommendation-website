51e78c082c65207113dc6124454e400feefd67c6
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-12-25 22:07:23 +0600

    display all the recommendations for a specific query

---
+++ src/Components/Recommendation.jsx
@@ -0,0 +1,30 @@
+const Recommendation = ({ recommendation }) => {
+  const { photo, product_name, current_time, reason, recommender_name } =
+    recommendation;
+  return (
+    <div className="max-w-md border border-yellow-500 bg-black text-white p-4 rounded-lg shadow-md">
+      <div className="flex items-center space-x-4">
+        {/* User Info */}
+        <div>
+          <h2 className="text-lg font-bold text-yellow-500">
+            {recommender_name}
+          </h2>
+          <p className="text-sm text-gray-400">{current_time}</p>
+        </div>
+      </div>
+      {/* User Message */}
+      <div className="my-2">
+        <img className="w-24 h-24 rounded-xl" src={photo} alt="" />
+        <h4>
+          Recommended Product:{" "}
+          <span className="text-yellow-500">{product_name}</span>
+        </h4>
+        <p>
+          Why should people choose it: <span className="text-yellow-500">{reason}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Recommendation;
--- src/Pages/QueryDetails/QueryDetails.jsx
+++ src/Pages/QueryDetails/QueryDetails.jsx
@@ -1,14 +1,24 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 import axios from "axios";
+import Recommendation from "../../Components/Recommendation";
 
 const QueryDetails = () => {
   const queryInfo = useLoaderData();
   const { user } = useContext(AuthContext);
-  console.log(queryInfo);
+  const [showRecommendation, setShowRecommendation] = useState([]);
   const { boycott, brand, name, createdAt, photo, title, _id, userInfo } =
     queryInfo;
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/recommend/${_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setShowRecommendation(data);
+      });
+  }, []);
 
   const handleRecommend = (e) => {
     e.preventDefault();
@@ -29,7 +39,7 @@ const QueryDetails = () => {
       recommender_email: user.email,
       user_name: userInfo.name,
       user_email: userInfo.email,
-      current_time : new Date()
+      current_time: new Date(),
     };
     console.log(recommendInfo);
 
@@ -44,92 +54,106 @@ const QueryDetails = () => {
       });
   };
   return (
-    <div className="flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto ">
-      {/* Job Details */}
-      <div className="flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]">
-        <div className="rounded-full object-cover overflow-hidden w-14 h-14">
-          <img src={userInfo?.userImage} alt="" />
-        </div>
-        <div>
-          <p className="mt-6 text-2xl font-semibold text-gray-600 ">
-            User Details:
-          </p>
-          <div className="flex items-center gap-5">
-            <div>
-              <p className="mt-2 text-xl font-semibold  text-gray-600 ">
-                Name: {userInfo?.name}
-              </p>
-              <p className="mt-2 text-xl font-semibold  text-gray-600 ">
-                Email: {userInfo?.email}
-              </p>
+    <div className=" py-8">
+      <div className="flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto">
+        {/* Job Details */}
+        <div className="flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]">
+          <div className="rounded-full object-cover overflow-hidden w-14 h-14">
+            <img src={userInfo?.userImage} alt="" />
+          </div>
+          <div>
+            <p className="mt-6 text-2xl font-semibold text-gray-600 ">
+              User Details:
+            </p>
+            <div className="flex items-center gap-5">
+              <div>
+                <p className="mt-2 text-xl font-semibold  text-gray-600 ">
+                  Name: {userInfo?.name}
+                </p>
+                <p className="mt-2 text-xl font-semibold  text-gray-600 ">
+                  Email: {userInfo?.email}
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <div>
+          <section className="p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]">
+            <h2 className="text-lg font-semibold text-gray-700 capitalize ">
+              Add your recommendation
+            </h2>
+
+            <form onSubmit={handleRecommend}>
+              <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <div>
+                  <label className="text-gray-700 " htmlFor="title">
+                    Recommendation Title
+                  </label>
+                  <input
+                    type="text"
+                    name="product_title"
+                    required
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                  />
+                </div>
+
+                <div>
+                  <label className="text-gray-700 " htmlFor="product_name">
+                    Recommended Product Name
+                  </label>
+                  <input
+                    type="text"
+                    name="product_name"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                  />
+                </div>
+
+                <div>
+                  <label className="text-gray-700 " htmlFor="photo">
+                    Recommended Product Image
+                  </label>
+                  <input
+                    name="photo"
+                    type="url"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                  />
+                </div>
+
+                <div>
+                  <label className="text-gray-700 " htmlFor="reason">
+                    Recommendation Reason
+                  </label>
+                  <input
+                    name="reason"
+                    type="text"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-end mt-6">
+                <button
+                  type="submit"
+                  className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                >
+                  Add Recommend
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
       </div>
-      <div>
-        <section className="p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]">
-          <h2 className="text-lg font-semibold text-gray-700 capitalize ">
-            Add your recommendation
-          </h2>
 
-          <form onSubmit={handleRecommend}>
-            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-              <div>
-                <label className="text-gray-700 " htmlFor="title">
-                  Recommendation Title
-                </label>
-                <input
-                  type="text"
-                  name="product_title"
-                  required
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-                />
-              </div>
-
-              <div>
-                <label className="text-gray-700 " htmlFor="product_name">
-                  Recommended Product Name
-                </label>
-                <input
-                  type="text"
-                  name="product_name"
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-                />
-              </div>
-
-              <div>
-                <label className="text-gray-700 " htmlFor="photo">
-                  Recommended Product Image
-                </label>
-                <input
-                  name="photo"
-                  type="url"
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-                />
-              </div>
-
-              <div>
-                <label className="text-gray-700 " htmlFor="reason">
-                  Recommendation Reason
-                </label>
-                <input
-                  name="reason"
-                  type="text"
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-end mt-6">
-              <button
-                type="submit"
-                className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-              >
-                Add Recommend
-              </button>
-            </div>
-          </form>
-        </section>
+      <div className="p-4 ">
+        <h4 className="text-3xl font-semibold">See All The Recommendations made by people</h4>
+      </div>
+      <div className="p-4 space-y-3">
+        {showRecommendation.map((recommendation) => (
+          <Recommendation
+            recommendation={recommendation}
+            key={recommendation._id}
+          ></Recommendation>
+        ))}
       </div>
     </div>
   );
