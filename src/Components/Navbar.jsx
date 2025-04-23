19825759cb791229f5d559bac461daba79beae28
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-04-24 02:29:55 +0600

    change the side bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ import Loading from "../Pages/Loading/Loading";
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const location = useLocation();
+  const [showSidebar, setShowSidebar] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -34,12 +35,12 @@ const Navbar = () => {
       >
         <div className="w-11/12 navbar mx-auto">
           <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
+            <div className="lg:hidden">
+              <button
+                onClick={() => setShowSidebar(true)}
+                className="btn btn-ghost"
               >
+                {" "}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -54,25 +55,90 @@ const Navbar = () => {
                     d="M4 6h16M4 12h8m-8 6h16"
                   />
                 </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              </button>
+              <div
+                className={`fixed inset-0 z-40 bg-black bg-opacity-30 transition-opacity duration-300 ${
+                  showSidebar ? "opacity-100 visible" : "opacity-0 invisible"
+                }`}
+                onClick={() => setShowSidebar(false)}
+              />
+              <div
+                className={`fixed top-0 left-0 z-50 h-full w-64 bg-base-100 shadow-md transform transition-transform duration-300 ${
+                  showSidebar ? "translate-x-0" : "-translate-x-full"
+                }`}
               >
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/queries">Queries</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/about">About Us</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contact">Contact Us</NavLink>
-                </li>
-              </ul>
+                {/* Close Button */}
+                <div className="flex justify-end p-4">
+                  <button onClick={() => setShowSidebar(false)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <ul className="menu p-4 space-y-2">
+                  <li>
+                    <Link
+                      to="/"
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full px-3 py-2 rounded-md transition-all duration-300 ${
+                        location.pathname === "/"
+                          ? "bg-[#62D0B4] text-white font-bold"
+                          : "font-bold"
+                      }`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/queries"
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full px-3 py-2 rounded-md transition-all duration-300 ${
+                        location.pathname === "/queries"
+                          ? "bg-[#62D0B4] text-white font-bold"
+                          : "font-bold"
+                      }`}
+                    >
+                      Queries
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/about"
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full px-3 py-2 rounded-md transition-all duration-300 ${
+                        location.pathname === "/about"
+                          ? "bg-[#62D0B4] text-white font-bold"
+                          : "font-bold"
+                      }`}
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/contact"
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full px-3 py-2 rounded-md transition-all duration-300 ${
+                        location.pathname === "/contact"
+                          ? "bg-[#62D0B4] text-white font-bold"
+                          : "font-bold"
+                      }`}
+                    >
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
+
             <img className="w-8 lg:flex hidden" src={icon} alt="" />
             <a className="lg:text-xl text-lg font-bold ml-[4px]">Recommendo</a>
           </div>
@@ -134,7 +200,7 @@ const Navbar = () => {
                 <Link to="/login" className="w-full">
                   <button
                     onClick={userSignOut}
-                    className={`px-6 p-2 hover:transition-all hover:shadow-xl hover:duration-500 rounded-full bg-[#62D0B4] text-[#fff] font-bold`}
+                    className={`lg:px-6 px-3 lg:p-2 hover:transition-all hover:shadow-xl hover:duration-500 rounded-full bg-[#62D0B4] text-[#fff] lg:text-base text-sm font-bold`}
                   >
                     Logout
                   </button>
@@ -182,7 +248,7 @@ const Navbar = () => {
             ) : (
               <Link to="/login">
                 <button
-                  className={`px-6 p-2 hover:transition-all hover:shadow-xl hover:duration-500 rounded-full bg-[#62D0B4] text-[#fff] font-bold`}
+                  className={`lg:px-6 px-3 py-2 hover:transition-all hover:shadow-xl hover:duration-500 rounded-full bg-[#62D0B4] text-[#fff] lg:text-base text-sm font-bold`}
                 >
                   Login
                 </button>
